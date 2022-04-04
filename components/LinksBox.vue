5df7c8c73f543bd766b40a3c3a41673e1de3fd86
Tiago Matos <tiago@tiagomatos.com>
2022-04-04 16:11:12 +1000

    Sort link

--- components/LinksBox.vue
+++ components/LinksBox.vue
@@ -7,27 +7,6 @@
 
         <div class="grid grid-cols-2 gap-4">
             <Card
-                v-for="link in links"
-                :key="link.label"
-                tag="a"
-                :href="link.href"
-                target="_blank"
-                class="flex items-center space-x-3 rounded-lg px-3 py-3"
-                :class="{ 'text-teal-400 dark:text-teal-400 font-bold': link.label === 'Escola LaraVue' }"
-            >
-                <div class="flex-none flex items-center justify-center w-8 h-8">
-                    <img
-                        :src="link.img"
-                        :alt="link.img_alt"
-                    >
-                </div>
-
-                <div class="text-xs md:text-sm truncate">
-                    {{ link.label }}
-                </div>
-            </Card>
-
-            <Card
                 tag="a"
                 href="https://escola.laravue.com.br/"
                 target="_blank"
@@ -60,6 +39,27 @@
                     Escola LaraVue
                 </div>
             </Card>
+
+            <Card
+                v-for="link in links"
+                :key="link.label"
+                tag="a"
+                :href="link.href"
+                target="_blank"
+                class="flex items-center space-x-3 rounded-lg px-3 py-3"
+                :class="{ 'text-teal-400 dark:text-teal-400 font-bold': link.label === 'Escola LaraVue' }"
+            >
+                <div class="flex-none flex items-center justify-center w-8 h-8">
+                    <img
+                        :src="link.img"
+                        :alt="link.img_alt"
+                    >
+                </div>
+
+                <div class="text-xs md:text-sm truncate">
+                    {{ link.label }}
+                </div>
+            </Card>
         </div>
     </div>
 </template>
@@ -71,13 +71,13 @@
         data() {
             return {
                 links: [
-                    { label: 'Instagram', img: require('@/assets/img/instagram.svg'), img_alt: 'Instagram @tiagomatosweb', href: 'https://www.instagram.com/tiagomatosweb/' },
+                    { label: 'LaraVue Discord', img: require('@/assets/img/discord.svg'), img_alt: 'LaraVue Discord', href: 'https://discord.gg/HXGwssB8S4' },
+                    { label: 'LaraVue Telegram', img: require('@/assets/img/telegram.svg'), img_alt: 'LaraVue Telegram', href: 'https://t.me/+YhayoHB107AK4kM_' },
                     { label: 'YouTube', img: require('@/assets/img/youtube.svg'), img_alt: 'Youtube /tiagomatosweb', href: 'https://www.youtube.com/tiagomatosweb?sub_confirmation=1' },
+                    { label: 'Instagram', img: require('@/assets/img/instagram.svg'), img_alt: 'Instagram @tiagomatosweb', href: 'https://www.instagram.com/tiagomatosweb/' },
                     // { label: 'Face', img: require('@/assets/img/facebook-f.svg'), img_alt: 'Facebook /tiagomatosweb', href: 'https://www.facebook.com/tiagomatosweb' },
                     // { label: 'Blog', img: require('@/assets/img/laptop.svg'), img_alt: 'Blog blog.tiagomatos.com', href: 'https://blog.tiagomatos.com/' },
                     // { label: 'Site', img: require('@/assets/img/globe.svg'), img_alt: 'Site tiagomatos.com', href: 'https://tiagomatos.com/' },
-                    { label: 'LaraVue Discord', img: require('@/assets/img/discord.svg'), img_alt: 'LaraVue Discord', href: 'https://discord.gg/HXGwssB8S4' },
-                    { label: 'LaraVue Telegram', img: require('@/assets/img/telegram.svg'), img_alt: 'LaraVue Telegram', href: 'https://t.me/+YhayoHB107AK4kM_' },
                     // { label: 'Escola LaraVue', img: require('@/assets/img/lv.svg'), img_alt: 'Escola LaraVue', href: 'https://escola.laravue.com.br/' },
                 ],
             };
