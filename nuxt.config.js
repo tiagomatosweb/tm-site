4e8be5785b84c4ec58794a725fad4835aa0b32f4
Tiago Matos <tiago@tiagomatos.com>
2022-06-12 10:40:21 -0300

    Remove eslint entirely

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,11 +48,7 @@ export default {
 
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
-        // https://go.nuxtjs.dev/eslint
         '@nuxtjs/eslint-module',
-        // https://go.nuxtjs.dev/stylelint
-        '@nuxtjs/stylelint-module',
-        // https://go.nuxtjs.dev/tailwindcss
         '@nuxtjs/tailwindcss',
         '@nuxtjs/google-analytics',
     ],
