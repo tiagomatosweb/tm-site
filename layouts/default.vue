502b69f9ef5f4c212c75bfb0ce13080b85966a4d
Tiago Matos <tiago@tiagomatos.com>
2023-05-01 16:45:41 +1000

    Attempt to fix vercel compilation bug 3

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,18 @@
 <template>
-    <div class="fixed top-0 inset-x-0 bg-tm-gradient h-[3px] w-full" />
+    <div>
+        <div class="fixed top-0 inset-x-0 bg-tm-gradient h-[3px] w-full" />
 
-    <div class="flex flex-col h-screen px-6 py-4">
-        <div>
-            <AppHeader />
+        <div class="flex flex-col h-screen px-6 py-4">
+            <div>
+                <AppHeader />
 
-            <div class="max-w-4xl mx-auto">
-                <slot />
+                <div class="max-w-4xl mx-auto">
+                    <slot />
+                </div>
             </div>
-        </div>
 
-        <AppFooter class="mt-auto" />
+            <AppFooter class="mt-auto" />
+        </div>
     </div>
 </template>
 
