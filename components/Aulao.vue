2dfe8cd8fa4e624adb13f152e8723938e66224c9
Tiago Matos <tiago@tiagomatos.com>
2023-07-24 20:25:25 +1000

    Update Aulao component

--- components/Aulao.vue
+++ components/Aulao.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
         <Heading tag="h2">
-            👨🏻‍💻&nbsp;&nbsp;<span class="tm-gradient">Aprenda a construir seu produto SaaS Multi-Tenant com Laravel API e Vue SPA</span>
+            👨🏻‍💻&nbsp;&nbsp;<span class="tm-gradient">Seu projeto SaaS do Zero ao Deploy com Laravel e Vue</span>
         </Heading>
-        <div class="space-y-1">
-            <p>🚀 Do zero do deploy</p>
-            <p>🗓️ Aulas toda quinta às 19h</p>
-            <p>🎥 Ao vivo no YouTube</p>
-<!--            <p>🔗 Toque aqui e ativa o sininho</p>-->
+
+        <p>Aprenda habilidades básicas e avançadas e abra portas para novas oportunidades. Acelere sua carreira dev ou torne-se um empreendedor de sucesso com seu próprio projeto SaaS.</p>
+
+        <div class="mt-2">
+            <a
+                href="https://laravue.com.br"
+                target="_blank"
+                class="inline-flex items-center gap-2 px-4 py-2 bg-tm-gradient text-gray-900 font-medium rounded-lg text-sm"
+            >Quero fazer meu SaaS <svg height="1em" viewBox="0 0 512 512"><path d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V32c0-17.7-14.3-32-32-32H352zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg></a>
         </div>
     </div>
 </template>
+<script setup lang="ts">
+</script>
