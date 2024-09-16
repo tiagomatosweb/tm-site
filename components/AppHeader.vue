8bfe730236627cda0ef538680149636975da7be4
Tiago Matos <tiago@tiagomatos.com>
2024-09-16 18:14:36 +1000

    Add newsletter wireframe

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,36 +1,37 @@
 <template>
-    <div class="flex items-center justify-between mb-14 mt-4 max-w-7xl mx-auto">
-        <div class="flex items-center space-x-3">
-            <NuxtLink :to="{ name: 'index' }">
-                <img
-                    src="@/assets/img/tm-logo-dark.svg"
-                    alt="Tiago Matos"
-                    width="40"
-                    class="inline-block dark:hidden"
-                >
+  <div class="flex items-center justify-between mb-14 mt-4 max-w-7xl mx-auto">
+    <div class="flex items-center space-x-3">
+      <NuxtLink :to="{ name: 'index' }">
+        <img
+          src="@/assets/img/tm-logo-dark.svg"
+          alt="Tiago Matos"
+          width="40"
+          class="inline-block dark:hidden"
+        >
 
-                <img
-                    src="@/assets/img/tm-logo-light.svg"
-                    alt="Tiago Matos"
-                    width="40"
-                    class="hidden dark:inline-block"
-                >
-            </NuxtLink>
-        </div>
-
-        <div class="flex items-center space-x-8">
-            <div class="flex items-center space-x-3">
-<!--                <NuxtLink :to="{ name: 'blog' }" class="main-menu">Blog</NuxtLink>-->
-                <NuxtLink to="https://laravue.com.br" class="main-menu" target="_blank">Cursos <span class="hidden sm:inline-block">Laravel e Vue</span></NuxtLink>
-            </div>
-
-            <div>
-                <ColorModeToggle />
-            </div>
-        </div>
+        <img
+          src="@/assets/img/tm-logo-light.svg"
+          alt="Tiago Matos"
+          width="40"
+          class="hidden dark:inline-block"
+        >
+      </NuxtLink>
     </div>
+
+    <div class="flex items-center space-x-8">
+      <div class="flex items-center space-x-3">
+        <!--                <NuxtLink :to="{ name: 'blog' }" class="main-menu">Blog</NuxtLink>-->
+        <NuxtLink to="https://laravue.com.br" class="main-menu" target="_blank">Cursos <span
+          class="hidden sm:inline-block">Laravel e Vue</span></NuxtLink>
+      </div>
+
+      <div>
+        <ColorModeToggle/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import ColorModeToggle from "~/components/ColorModeToggle.vue";
+import ColorModeToggle from '~/components/ColorModeToggle.vue';
 </script>
---
+++ layouts/lp.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="flex flex-col h-screen px-6 py-4">
+      <div>
+        <NuxtLink :to="{ name: 'index' }">
+          <img
+            src="@/assets/img/tm-logo-dark.svg"
+            alt="Tiago Matos"
+            width="40"
+            class="inline-block dark:hidden"
+          >
+
+          <img
+            src="@/assets/img/tm-logo-light.svg"
+            alt="Tiago Matos"
+            width="40"
+            class="hidden dark:inline-block"
+          >
+        </NuxtLink>
+
+        <div class="max-w-4xl mx-auto">
+          <slot/>
+        </div>
+      </div>
+
+      <AppFooter class="mt-auto"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AppFooter from '@/components/AppFooter.vue';
+import AppHeader from '@/components/AppHeader.vue';
+</script>
