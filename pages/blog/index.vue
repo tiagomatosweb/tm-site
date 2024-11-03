f6b9808064511444aba2bbc4743df634b8a606d9
Tiago Matos <tiago@tiagomatos.com>
2024-11-03 12:09:29 +1100

    Fix

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,34 +1,34 @@
 <template>
-    <div>
-        <BackBtn :to="{ name: 'index' }" />
+  <div>
+    <BackBtn :to="{ name: 'index' }"/>
 
-        <div class="flex flex-col space-y-4">
-            <Card
-                v-for="post in data"
-                :key="post._path"
-                class="relative p-4"
-            >
-                <div class="text-xs text-gray-500 tracking-wide">{{ $dayjs(post.date).format('DD MMMM YYYY') }}</div>
+    <div class="flex flex-col space-y-4">
+      <Card
+        v-for="post in data"
+        :key="post._path"
+        class="relative p-4"
+      >
+        <div class="text-xs text-gray-500 tracking-wide">{{ $dayjs(post.date).format('DD MMMM YYYY') }}</div>
 
-                <NuxtLink
-                    :to="post._path"
-                    class="text-lg font-bold text-gray-900 dark:text-gray-50 leading-4"
-                >
-                    {{ post.title }}
-                </NuxtLink>
+        <NuxtLink
+          :to="post._path"
+          class="text-lg font-bold text-gray-900 dark:text-gray-50 leading-4"
+        >
+          {{ post.title }}
+        </NuxtLink>
 
-                <div class="mt-1">
-                    <p class="text-sm text-gray-500">{{ post.description }}</p>
-                </div>
-            </Card>
+        <div class="mt-1">
+          <p class="text-sm text-gray-500">{{ post.description }}</p>
         </div>
+      </Card>
     </div>
+  </div>
 </template>
 
 <script setup>
-import Card from '@/components/TMCard.vue'
 import BackBtn from '~/components/BackBtn.vue';
-const { data } = await useAsyncData('blog', () => queryContent('/blog')
-    .sort({ date: -1 })
-    .find())
+
+const {data} = await useAsyncData('blog', () => queryContent('/blog')
+  .sort({date: -1})
+  .find())
 </script>
