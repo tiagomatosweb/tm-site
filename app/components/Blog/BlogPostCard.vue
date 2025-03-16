30b24bdf59423be483e36fb6cd7bc93d3d45011a
Tiago Matos <tiago@tiagomatos.com>
2025-03-16 14:42:06 +1100

    Fix blog card

--- app/components/Blog/BlogPostCard.vue
+++ app/components/Blog/BlogPostCard.vue
@@ -2,30 +2,26 @@
   <NuxtLink
     :key="post.path"
     :to="post.path"
+    class="p-4 border border-gray-200 dark:border-gray-800 bg-transparent hover:bg-gray-100 dark:bg-transparent dark:hover:bg-gray-900"
   >
-    <Card
-      class="p-4 border border-gray-200 dark:border-gray-800 bg-transparent hover:bg-gray-100 dark:bg-transparent dark:hover:bg-gray-900">
-      <div class="text-xs text-gray-500 tracking-wide">{{ $dayjs(post.date).format('DD MMMM YYYY') }}</div>
+    <div class="text-xs text-gray-500 tracking-wide">{{ $dayjs(post.date).format('DD MMMM YYYY') }}</div>
 
-      <h3 class="font-semibold mt-2">
-        {{ post.title }}
-      </h3>
+    <h3 class="font-semibold mt-2">
+      {{ post.title }}
+    </h3>
 
-      <div class="mt-2">
-        <p class="text-sm text-gray-500">{{ post.description }}</p>
-      </div>
+    <div class="mt-2">
+      <p class="text-sm text-gray-500 dark:text-gray-400">{{ post.description }}</p>
+    </div>
 
-      <div class="mt-4 flex items-center text-sm font-medium text-primary">
-        Leia mais
-        <ChevronRightIcon class="size-4 ml-1"/>
-      </div>
-    </Card>
+    <div class="mt-4 flex items-center text-sm font-medium text-(--ui-primary)">
+      Leia mais
+      <UIcon name="i-lucide-chevron-right" class="size-4 ml-1"/>
+    </div>
   </NuxtLink>
 </template>
 
 <script setup>
-import {ChevronRightIcon} from 'lucide-vue-next';
-
 const props = defineProps({
   post: {
     type: Object,
